0c812465ba399b1e08579f33094a8b4b89a0ddff
Wei <674501104@qq.com>
2020-07-19 23:05:27 +0800

    快速记忆

--- AttentionTraining/utils/util.js
+++ AttentionTraining/utils/util.js
@@ -24,7 +24,14 @@ function sortArray(arr) {
   return arr
 }
 
+//获取px与rpx之间的比列 
+function getRpx() { 
+  var winWidth = wx.getSystemInfoSync().windowWidth;
+   return 750/winWidth;
+}
+
 module.exports = {
   formatTime: formatTime,
-  sortArray: sortArray
+  sortArray: sortArray,
+  getRpx: getRpx
 }
