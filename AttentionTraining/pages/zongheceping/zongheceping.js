ddc4f5dc1a7bc162f9d5a89123a48de59d70c7a8
Wei <674501104@qq.com>
2020-08-16 21:32:52 +0800

    测评结果修复

--- AttentionTraining/pages/zongheceping/zongheceping.js
+++ AttentionTraining/pages/zongheceping/zongheceping.js
@@ -96,11 +96,36 @@ Page({
       key: 'userInfo',
       success: function (res) {
         if (res.data) {
+          that.setData({
+            userData: {
+              userName: res.data.nickName,
+              sex: (res.data.gender==1)?"男":"女",
+              age: "",
+              mobile: ""
+            },
+          })
           api.getCombinationEvaluationByOpenid({
             data: { openid: res.data.openid },
             success: function (result) {
-              if (result.data) { 
-                
+              if (result.code == 1) { 
+                that.setData({
+                  deFen: {
+                    headers:[{ text: 'one', display: '类型' }, { text: 'three', display: '用时' }, { text: 'four', display: '正确数' }, { text: 'five', display: '正确率' }, { text: 'six', display: '得分' }],
+                    row: [
+                        {'one': '交通灯','three': '-','four': '-','five': result.data.percentage0,'six': result.data.score0},
+                        {'one': '小符号','three': '-','four': '-','five': result.data.percentage1,'six': result.data.score1},
+                        {'one': '快速记忆','three': '-','four': '-','five': result.data.percentage2,'six': result.data.score2},
+                        {'one': '斯特如普','three': '-','four': '-','five': result.data.percentage3,'six': result.data.score3}]
+                  },
+                  tiXian: {
+                    headers:[{ text: 'one', display: '类型' }, { text: 'two', display: '反应能力' }, { text: 'three', display: '认知灵活度' }, { text: 'four', display: '信息处理能力' }, { text: 'five', display: '记忆能力' }],
+                    row: [
+                        {'one': '分数','two': '-','three': '-','four': '-','five': '-'},
+                        {'one': '标准分数','two': '80','three': '80','four': '80','five': '80'},
+                        {'one': '百分比','two': '-','three': '-','four': '-','five': '-'},
+                        {'one': '说明','two': '反应能力，是指在接受到信息之后，经过大脑对信息的分析、处理之后，作出正确决策的能力','three': '认知灵活度，是指在完成复杂的认知任务时，对各种认知过程进行协调，灵活的根据不同的要求进行状态的切换，以保证目标的顺利完成能力','four': '信息处理能力是指孩子在接收一系列复杂的信息时，大脑经过排列、组合、加工逻辑推理之后得到结果的能力','five': '记忆能力主要指在大脑接收到语言及图像信息时，大脑对信息的记忆能力，记忆能力强弱直接影响学习能力强弱'}]
+                  },
+                })
               }
             },
             fail: function (res) {
